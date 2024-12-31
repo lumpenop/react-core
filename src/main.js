1a33efb3ba74d0987f3f65d9a9a1456993e10d1d
lumpen <swk5373@gmail.com>
2025-01-01 06:52:25 +0900

    week3 - useEffect 추가 - App() 내의 state 유지 방법을 찾지 못하여 변경점만 찾는 부분은 적용 못함

--- src/main.js
+++ src/main.js
@@ -5,7 +5,7 @@ import React from './utils/react.js'
 // 초기 렌더링
 
 const app = document.getElementById('app')
-const rootElement = React.renderComponent(App)
+const rootElement = React.renderComponent(App())
 const { eventListeners } = React
 const eventsObj = {}
 
@@ -29,7 +29,29 @@ eventKeys.forEach(event => {
 app.appendChild(rootElement)
 // 상태 변경 시 재렌더링을 위한 리스너 등록
 const unsubscribe = React.subscribe(() => {
-  const newRootElement = React.renderComponent(App)
+  const newRootElement = React.renderComponent(App())
+  const { eventListeners } = React
+  const eventsObj = {}
+
+  eventListeners.forEach(listener => (eventsObj[listener.event] = []))
+  eventListeners.forEach(listener =>
+    eventsObj[listener.event].push({ target: listener.target, listener: listener.listener })
+  )
+
+  const eventKeys = Object.keys(eventsObj)
+  eventKeys.forEach(event => {
+    console.log(event, 'event')
+    app.addEventListener(`${event}`, e => {
+      eventsObj[event].forEach(listener => {
+        if (listener.target === e.target) {
+          listener.listener(e)
+        }
+      })
+    })
+  })
+
+  app.innerHTML = ''
+  app.appendChild(newRootElement)
 })
 
 React.subscribe(() => {})
